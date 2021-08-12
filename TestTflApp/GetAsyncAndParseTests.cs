4ea2404ad132cd41735542be53e213be6ad28b7e
Keerthi Jagalur Mutt <kjagalurmutt@uk.imshealth.com>
2021-08-12 23:39:26 +0100

    Adding Positive and Negetive Test Cases

--- TestTflApp/GetAsyncAndParseTests.cs
+++ TestTflApp/GetAsyncAndParseTests.cs
@@ -7,6 +7,7 @@ using Moq.Protected;
 using Newtonsoft.Json.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using TflApp;
@@ -16,7 +17,10 @@ namespace TestTflApp
     [TestClass]
     public class GetAsyncAndParseTests
     {
-
+        private readonly string[] invalidRoad = new string[1] { "A233" };
+        private readonly string[] validRoad = new string[1] { "A2" };
+        private readonly string jsonArrayValidRoadResponse = "[{ \"type\":\"Tfl.Api.Presentation.Entities.RoadCorridor, Tfl.Api.Presentation.Entities\",\"id\":\"a2\", \"displayName\":\"A2\",\"statusSeverity\":\"Good\",\"statusSeverityDescription\":\"No Exceptional Delays\", \"bounds\":\"[[-0.0857,51.44091],[0.17118,51.49438]]\",\"envelope\":\"[[-0.0857,51.44091],[-0.0857,51.49438],[0.17118,51.49438],[0.17118,51.44091],[-0.0857,51.44091]]\",\"url\":\"/Road/a2\" }]";
+        private readonly string jsonObjectInvalidRoadResponse = @"{ ""type"":""Tfl.Api.Presentation.Entities.ApiError, Tfl.Api.Presentation.Entities"",""timestampUtc"":""2017-11-21T14:37:39.7206118Z"", ""exceptionType"":""EntityNotFoundException"",""httpStatusCode"":""404"",""httpStatus"":""NotFound"", ""relativeUri"":""/Road/A233"",""message"":""The following road id is not recognised: A233"" }";
         Mock<ILogger<ApiService>> logger;
         Mock<IHttpClientFactory> mockFactory;
         Mock<HttpMessageHandler> mockHttpMessageHandler;
@@ -36,6 +40,60 @@ namespace TestTflApp
             IConfigurationSection url = configuration.GetSection(CommonConstants.URL);
             string path = url.Value.Replace(CommonConstants.ARG1, args[0]).Replace(CommonConstants.ARG2, app_id.Value).Replace(CommonConstants.ARG3, app_key.Value);
             return path;
+        }
+
+        [TestMethod]
+        public async Task NegativeTest()
+        {
+            //arrange   
+            string url = GetUrl(invalidRoad);
+            HttpResponseMessage httpResponse = new();
+            httpResponse.StatusCode = HttpStatusCode.OK;
+            httpResponse.Content = new StringContent(jsonObjectInvalidRoadResponse, Encoding.UTF8, "application/json");
+
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.Is<HttpRequestMessage>(r => r.Method == HttpMethod.Get && r.RequestUri.ToString().StartsWith(url)),
+                ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(httpResponse);
+
+            HttpClient httpClient = new(mockHttpMessageHandler.Object);
+            mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+            ApiService service = new(logger.Object, mockFactory.Object);
+
+            //act
+            var actualResults = await service.GetAsync(url);
+
+            //assert
+            Assert.AreEqual(jsonObjectInvalidRoadResponse, actualResults);
+        }
+
+        [TestMethod]
+        public async Task PositiveTest()
+        {
+            //arrange   
+            string url = GetUrl(validRoad);
+            HttpResponseMessage httpResponse = new();
+            httpResponse.StatusCode = HttpStatusCode.OK;
+            httpResponse.Content = new StringContent(jsonArrayValidRoadResponse, Encoding.UTF8, "application/json");
+
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.Is<HttpRequestMessage>(r => r.Method == HttpMethod.Get && r.RequestUri.ToString().StartsWith(url)),
+                ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(httpResponse);
+
+            HttpClient httpClient = new(mockHttpMessageHandler.Object);
+            mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+            ApiService service = new(logger.Object, mockFactory.Object);
+
+            //act
+            var actualResults = await service.GetAsync(url);
+
+            //assert
+            Assert.AreEqual(jsonArrayValidRoadResponse, actualResults);
         }
 
         [TestMethod]
@@ -77,9 +135,8 @@ namespace TestTflApp
 
         [TestMethod]
         public void ShouldJsonArrayParseCorrectly()
-        {
-            string jsonArray = "[{ \"type\":\"Tfl.Api.Presentation.Entities.RoadCorridor, Tfl.Api.Presentation.Entities\",\"id\":\"a2\", \"displayName\":\"A2\",\"statusSeverity\":\"Good\",\"statusSeverityDescription\":\"No Exceptional Delays\", \"bounds\":\"[[-0.0857,51.44091],[0.17118,51.49438]]\",\"envelope\":\"[[-0.0857,51.44091],[-0.0857,51.49438],[0.17118,51.49438],[0.17118,51.44091],[-0.0857,51.44091]]\",\"url\":\"/Road/a2\" }]";
-            JToken token = JToken.Parse(jsonArray);
+        {            
+            JToken token = JToken.Parse(jsonArrayValidRoadResponse);
             Road road = JsonTokenReaderUtility.ConvertToObject(token);
             var validRoad = (ValidRoad)road;
             Assert.AreEqual("Tfl.Api.Presentation.Entities.RoadCorridor, Tfl.Api.Presentation.Entities", road.type);
@@ -88,9 +145,8 @@ namespace TestTflApp
 
         [TestMethod]
         public void ShouldJsonObjectParseCorrectly()
-        {
-            string jsonObject = "{ \"type\":\"Tfl.Api.Presentation.Entities.ApiError, Tfl.Api.Presentation.Entities\",\"timestampUtc\":\"2017-11-21T14:37:39.7206118Z\", \"exceptionType\":\"EntityNotFoundException\",\"httpStatusCode\":\"404\",\"httpStatus\":\"NotFound\", \"relativeUri\":\"/Road/A233\",\"message\":\"The following road id is not recognised: A233\" }";
-            JToken token = JToken.Parse(jsonObject);
+        {           
+            JToken token = JToken.Parse(jsonObjectInvalidRoadResponse);
             Road road = JsonTokenReaderUtility.ConvertToObject(token);          
             Assert.AreEqual("Tfl.Api.Presentation.Entities.ApiError, Tfl.Api.Presentation.Entities", road.type);
             var invalidRoad = (InvalidRoad)road;           
@@ -98,5 +154,7 @@ namespace TestTflApp
             Assert.AreEqual("404", invalidRoad.httpStatusCode);
         }
 
+
+       
     }
 }
